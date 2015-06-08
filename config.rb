bc30c30009eeeaa47d018f99c1905c72eba4cf92
LuaxY <yann.guineau@voidmx.net>
2015-06-08 13:49:37 +0200

    Base template

---
+++ config.rb
@@ -0,0 +1,9 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "imgs"
+javascripts_dir = "js"
+line_comments = false
+relative_assets = true
